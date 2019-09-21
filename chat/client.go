0c3191eda9b9e10e7d09c6500788c622bd721725
Daichi Kamiyama <dawt2h@gmail.com>
2019-09-21 18:59:28 +0900

    チャットの入退室管理

--- chat/client.go
+++ chat/client.go
@@ -27,5 +27,11 @@ func (c *client) read() {
 }
 
 func (c *client) write() {
-	for msg := range c.send
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg);
+				err != nil {
+					break
+				}
+	}
+	c.socket.Close()
 }
