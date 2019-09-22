0cf0967131792a97fa8a6292090e87299d6172e5
Daichi Kamiyama <dawt2h@gmail.com>
2019-09-23 00:19:41 +0900

    チャットルーム機能完成

--- chat/main.go
+++ chat/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"flag"
 	"net/http"
 	"path/filepath"
 	"sync"
@@ -22,10 +23,22 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			template.Must(template.ParseFiles(filepath.Join("templates",
 				t.filename)))
 	})
-	t.temp1.Execute(w, nil)
+	t.temp1.Execute(w, r)
 }
 
 func main() {
+		var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+		flag.Parse() //フラグを解釈する
+		r := newRoom()
+		http.Handle("/", &templateHandler{filename: "chat.html"})
+		http.Handle("/room", r)
+		// チャットルームを開始します
+		go r.run()
+		// Webサーバーを起動します
+		log.Println("webサーバーを開始します。ポート：", *addr)
+		if err := http.ListenAndServe(*addr, nil); err != nil {
+			log.Fatal("ListenAndServe:", err)
+		}
 		// ルートアクセスハンドラー
 		http.Handle("/", &templateHandler{filename: "chat.html"}) 
 
